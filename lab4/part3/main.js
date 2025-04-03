4fd1a55b79d737f57bc1e48f35cb2ae12176ea2f
Daniel Mitchell <biomitchy@gmail.com>
2025-04-03 12:52:04 -0400

    Assign 4 Part 3 - Object building
    I cleaned up the script. I reorganized the initial provided script for enhanced readability and flow.
    
    Setup the canvas, set its dimensions, and remade the utility functions.

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -7,21 +7,20 @@ Description: Lab 4 Part 3 - Object building practice: LET'S BOUNCE SOME BALLS!!!
 
 */
 
-// setup canvas
+/* 1. Choosing the canvas and set up. */
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
+/* 2. Setting the dimensions of the canvas. */
 
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const width = canvas.width;
+const height = canvas.height;
 
-// function to generate random color
+/* 3. Remaking the needed functions. */
 
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const randomRGB = () => `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+
