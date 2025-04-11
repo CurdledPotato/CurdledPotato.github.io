64019c58fc1872127a29ec45e3ee2d39fc9cba58
Daniel Mitchell <biomitchy@gmail.com>
2025-04-11 17:06:18 -0400

    Lab 5 Part 1 - 25th commit
    Editing by restructuring and reorganizing the JavaScript file. Temporarily removed functionality for changes. The ability to hide the comment was added and the show or hide button comment wrapper elements as well. The first states  for accessibility are added.

--- lab5/part1/main.js
+++ lab5/part1/main.js
@@ -7,50 +7,13 @@ Description: Lab 5 Part 1 - Challenge: Accessibility troubleshooting
 
 */
 
-// functionality for showing/hiding the comments section
+// 1. Accessible show or hide comment functionality with enhancements and the elements for the comment wrapper and show or hide button.
 
 const showHideBtn = document.querySelector('.show-hide');
 const commentWrapper = document.querySelector('.comment-wrapper');
 
-commentWrapper.style.display = 'none';
+// 2. The initial states for accessibility.
 
-showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
-    showHideBtn.textContent = 'Hide comments';
-    commentWrapper.style.display = 'block';
-  } else {
-    showHideBtn.textContent = 'Show comments';
-    commentWrapper.style.display = 'none';
-  }
-};
-
-// functionality for adding a new comment via the comments form
-
-const form = document.querySelector('.comment-form');
-const nameField = document.querySelector('#name');
-const commentField = document.querySelector('#comment');
-const list = document.querySelector('.comment-container');
-
-form.onsubmit = function(e) {
-  e.preventDefault();
-  submitComment();
-};
-
-function submitComment() {
-  const listItem = document.createElement('li');
-  const namePara = document.createElement('p');
-  const commentPara = document.createElement('p');
-  const nameValue = nameField.value;
-  const commentValue = commentField.value;
-
-  namePara.textContent = nameValue;
-  commentPara.textContent = commentValue;
-
-  list.appendChild(listItem);
-  listItem.appendChild(namePara);
-  listItem.appendChild(commentPara);
-
-  nameField.value = '';
-  commentField.value = '';
-}
+commentWrapper.setAttribute('hidden', true);
+showHideBtn.setAttribute('aria-expanded', 'false');
+showHideBtn.setAttribute('tabindex', '0');
